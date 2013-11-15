2e50ae8a4e8ddfb89bc38e953b47f3a8c2dd079c
Mark McAulay <markmc@eqdesign.co.uk>
2013-11-15 15:15:04 +0000

    tweaking stuff. take more away

--- public/server.js
+++ public/server.js
@@ -1,1 +1,6 @@
-var connect=require("connect"),app=connect().use(connect.logger("dev")).use(connect.static("public")).listen(process.env.PORT||3e3);
+var connect = require('connect');
+
+var app = connect()
+  .use(connect.logger('dev'))
+  .use(connect.static('public'))
+  .listen(process.env.PORT || 3000);
